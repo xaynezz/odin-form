c3afcf4cd2a30b5c19a7d4594dfe109e581c3f27
Andrew <andrewoaksoekhant@gmail.com>
2022-07-07 22:16:52 +0800

    fixed e bug and added confirm pass logic

--- main.js
+++ main.js
@@ -6,23 +6,41 @@ function isEmpty() {
             pass = false;
         }
     });
-    
+
     if (!pass) {
-        alert("Please enter and pass all fields before submitting")
+        alert("Please enter and pass all fields before submitting");
+        return false;
+    }
+    
+    if (!email.checkValidity()) {
+        alert("Please enter valid email");
+        return false;
+    }
+
+    if (!phone.checkValidity()) {
+        alert("Please enter SG mobile number starting with 9/8");
         return false;
     }
     
     if (pass1.value !== pass2.value) {
-        alert("Passwords do not match. Try again!")
+        alert("Passwords do not match. Try again!");
         return false;
     }
-
+    
     alert("Form submitted")
     return true;
 }
 
 let submitBtn = document.querySelector(".submit");
-submitBtn.addEventListener("click", isEmpty);
-
 let pass1 = document.querySelector("#password1");
 let pass2 = document.querySelector("#password2");
+let email = document.querySelector("#email");
+let phone = document.querySelector("#phoneNumber");
+
+submitBtn.addEventListener("click", isEmpty);
+pass2.addEventListener("change", () => {
+    if (pass2.value !== pass1.value)
+        pass2.classList.add("fail");
+    else
+        pass2.classList.remove("fail");
+})
